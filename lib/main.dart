3bbe886f4dffabca4979adcd4c10f2da382d454a
Marlon Chiodelli <marlon.chiodelli@hotmail.com>
2020-10-12 20:21:52 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:chat_app/chat-screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+    runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.blue)
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
